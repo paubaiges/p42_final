c4ace6f546e44a9238fb41d3c4ca0a8c0588176c
Simone <simonegigante17@gmail.com>
2022-06-06 10:43:46 +0200

    microphone update

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,29 +1,24 @@
-import React, { Component } from "react";
-import './Game.css';
+import React, { Component, useEffect, useRef, useState } from "react";
+import './App.css';
+import { ReactMediaRecorder } from "react-media-recorder";
+
+
 
 function Game(){
     return(
-        <head>
-            <link rel="stylesheet" href="Game.css"/>
-        </head>,
-        <body> 
-            <nav>
-            <label className="logo">BeatMe!</label>
-                <ul>
-                    <li><a href='/'>Home</a></li>
-                    <li className="active"><a className="active" href="/game">Game</a></li>
-                    <li><a href='/ranking'>Ranking</a></li>
-                </ul>  
-            </nav>
-            <h1>Play the game!</h1>
-            <br></br>
-            <h2>Rhythm number 1</h2>
-            <br></br>
-            <br></br>
-            <button className="btn btn2" type="button">REC</button>
-            <h2>Score</h2>
-        </body>
+        <div>
+        <ReactMediaRecorder
+          audio
+          render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+            <div>
+              <p>{status}</p>
+              <button onClick={startRecording}>Start Recording</button>
+              <button onClick={stopRecording}>Stop Recording</button>
+              <audio src={mediaBlobUrl} controls autoPlay loop />
+            </div>
+          )}
+        />
+      </div>
     );
 }
-
 export default Game;
